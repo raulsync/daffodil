825ff6523a8860c1575dc6f18db6145d15cc36de
raulsync <rahul.aanand086@gmail.com>
2024-09-02 22:22:40 +0530

    herosection_completed

---
+++ src/components/herosection/HeroSection.jsx
@@ -0,0 +1,20 @@
+function HeroSection() {
+  return (
+    <>
+      <div className="hero-section h-[90vh] w-full bg-gray-50 flex items-center justify-center relative px-8">
+        <div className="content text-center">
+          <h1 className="text-5xl font-bold text-yellow-600">
+            Your trusted partner for software development excellence
+          </h1>
+          <p className="text-gray-600 text-lg mt-4">
+            Efficiently transform your vision into a market-ready software
+            application that stands out for its robustness, reliability, and
+            user experience.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
