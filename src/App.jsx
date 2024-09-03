ed4b6c5a00c2e6fabd6f4f59faca4304d2c16ec5
raulsync <rahul.aanand086@gmail.com>
2024-09-03 16:41:59 +0530

    carousel_image_react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HeroSection from './components/herosection/HeroSection';
+import MissionPage from './components/missionPage/MissionPage';
 import NavBar from './components/navBar/NavBar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="app h-[100vh]">
         <NavBar />
         <HeroSection />
+        <MissionPage />
       </div>
     </>
   );
