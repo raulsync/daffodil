0f9b7bea11e78bd413587c515b6d17393945aad7
raulsync <rahul.aanand086@gmail.com>
2024-09-03 18:17:34 +0530

    hero-section_fixed

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -1,12 +1,19 @@
 function HeroSection() {
   return (
     <>
-      <div className="hero-section h-[90vh] w-full bg-gray-50 flex items-center justify-center relative px-8">
-        <div className="content text-center">
+      <div
+        className="hero-section h-[100vh] w-full bg-gray-50 flex items-center justify-center relative px-8"
+        style={{
+          backgroundImage:
+            "url('https://images.pexels.com/photos/3184299/pexels-photo-3184299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+          backgroundSize: '100% 100%',
+        }}
+      >
+        <div className="content text-center w-full flex justify-center items-center flex-col">
           <h1 className="text-5xl font-bold text-yellow-600">
             Your trusted partner for software development excellence
           </h1>
-          <p className="text-gray-600 text-lg mt-4">
+          <p className="text-black w-1/2 text-lg mt-4">
             Efficiently transform your vision into a market-ready software
             application that stands out for its robustness, reliability, and
             user experience.
